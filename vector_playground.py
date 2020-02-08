c6c3015e02cef7e7405e5ee7af01d73b1098591a
Gregory Sizemore <tarwatirno@gmail.com>
2020-02-07 17:09:25 -0800

    Clean up formatting, fix vector sumation, add prettyprinter

--- vector_playground.py
+++ vector_playground.py
@@ -1,82 +1,27 @@
-
 # coding: utf-8
-
-# In[1]:
-
-
 import numpy as np
 from collections import Counter
+import pprint
 
-
-# In[66]:
-
-
+pp = pprint.PrettyPrinter(indent=4)
 with open("clean.txt") as file:
     contents = file.read()
 
-
-# In[67]:
-
-
 characters = set(contents)
 table = {}
-
-
-# In[68]:
-
-
 np.random.seed(76)
 for c in characters:
-    table[c] = np.random.choice([0, 1], size=(100000,), p=[1999./2000, 1./2000])
+    table[c] = np.random.choice([-1, 1], size=(100000,))
+counts = Counter(contents)
 
-
-# In[69]:
-
-
-count = Counter(contents)
-
-
-# In[70]:
-
-
-summation = np.zeros(100000)
-
-
-# In[71]:
-
-
-for t in table:
-    summation = np.add(summation, np.multiply(count[t], table[t]))
-
-
-# In[72]:
-
-
-temp = ' '
-x = np.dot(summation, table[temp])
-x / np.sum(table[temp]) / count[temp]
-
+est_counts = np.zeros(100000)
+for c in contents:
+    est_counts = np.add(est_counts, table[c])
 
 # In[73]:
-
-
 freq_est = {}
-for t in table:
-    x = np.dot(summation, table[t])
-    freq_est[t] = x / np.sum(table[t]) / count[t]
+for c in table:
+    est_count = np.dot(est_counts, table[c])
+    freq_est[c] = (est_count, counts[c])
 
-
-# In[74]:
-
-
-avg = 0
-for t in table:
-    avg += freq_est[t]
-avg / len(table)
-
-
-# In[75]:
-
-
-freq_est
-
+pp.pprint(freq_est)
