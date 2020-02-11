9c032b8e67f51580285be9070339b5b917a30474
Gregory Sizemore <tarwatirno@gmail.com>
2020-02-11 08:40:21 -0800

    Reorganize file into functions

--- vector_playground.py
+++ vector_playground.py
@@ -7,21 +7,22 @@ pp = pprint.PrettyPrinter(indent=4)
 with open("clean.txt") as file:
     contents = file.read()
 
-characters = set(contents)
+
+counts = Counter(contents)
 table = {}
 np.random.seed(76)
-for c in characters:
-    table[c] = np.random.choice([-1, 1], size=(100000,))
-counts = Counter(contents)
+for c in counts:
+    table[c] = np.random.choice([-1, 1], size=(10000,))
 
-est_counts = np.zeros(100000)
-for c in contents:
-    est_counts = np.add(est_counts, table[c])
+def estimate_counts():
+    counts_v = np.zeros(10000)
+    for c in contents:
+        counts_v = np.add(counts_v, table[c])
 
-# In[73]:
-freq_est = {}
-for c in table:
-    est_count = np.dot(est_counts, table[c])
-    freq_est[c] = (est_count, counts[c])
+    freq_est = {}
+    for c in table:
+        est_count = np.dot(counts_v, table[c])
+        freq_est[c] = (est_count, counts[c])
 
-pp.pprint(freq_est)
+    pp.pprint(freq_est)
+
