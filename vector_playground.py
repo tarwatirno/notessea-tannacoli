75e6d70a2031fb9e8db0239bb23ff4d6bc80d6c8
Gregory Sizemore <tarwatirno@gmail.com>
2020-02-11 18:23:54 -0800

    Add context-grams

--- vector_playground.py
+++ vector_playground.py
@@ -1,9 +1,9 @@
 # coding: utf-8
 import numpy as np
 from collections import Counter
-
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
+import copy
 
 with open("clean.txt") as file:
     doc = file.read()
@@ -34,3 +34,11 @@ def ngrams(n):
         for j in range(n-1, 0, -1):
             ngrams = np.add(np.roll(table[doc[i-j]], -j), ngrams)
     return ngrams
+def skip3gram_table():
+    skiptable = copy.deepcopy(table)
+    skip3grams = np.zeros(10000)
+    for i in range(2, len(doc)):
+        c = doc[i-1]
+        for j in [0, 2]:
+            skiptable[c] = np.add(np.roll(table[doc[i-j]], -j), skiptable[c])
+    return skiptable
